5400dd9645052f7dc9fb23605a4ddeab54c11d2d
DaniloHY <nilo.11.dhy@gmail.com>
2023-03-14 21:28:58 -0300

    Adaptador

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
@@ -1,7 +1,18 @@
 package br.com.etecia.recyclerviewfilmes;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyAdapter extends RecyclerView.Adapter<> {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
+
+    //criar a classe ViewHolder
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
