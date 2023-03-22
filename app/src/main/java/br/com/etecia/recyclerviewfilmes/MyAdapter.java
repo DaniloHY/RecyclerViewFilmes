37bed92651ace7004a3578824a8b47be7dc599dd
DaniloHY <nilo.11.dhy@gmail.com>
2023-03-21 21:30:29 -0300

    Funcionando

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
@@ -1,6 +1,7 @@
 package br.com.etecia.recyclerviewfilmes;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,7 +44,21 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         holder.idTituloFilmes.setText(lstFilmes.get(position).getTitulo());
         holder.idImagemFilmes.setImageResource(lstFilmes.get(position).getImagem());
 
+        holder.idCardFilmes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContexto, ApresentaFilmeActivity.class);
 
+                intent.putExtra("Titulo", lstFilmes.get(position).getTitulo());
+                intent.putExtra("Descricao", lstFilmes.get(position).getDescricao());
+                intent.putExtra("Categoria", lstFilmes.get(position).getCategoria());
+                intent.putExtra("ImagemFilme", lstFilmes.get(position).getImagem());
+
+                mContexto.startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+
+
+            }
+        });
     }
 
 
